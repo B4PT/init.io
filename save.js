dd2b04dbfbd39e677ce8c6d604296e964486df16
B4PT <30923509+B4PT@users.noreply.github.com>
2020-01-17 21:06:30 +0100

    Update save.js

--- save.js
+++ save.js
@@ -105,3 +105,35 @@ class MaClasse {
 }
 
 const monObjet = new MaClasse(arg1, arg2, ...);
+                              
+                              // Ajoutez votre code ici
+class Chien {
+  constructor(nom, race, taille){
+    this.nom = nom;
+    this.race = race;
+    this.taille = taille;
+  }
+  decrire(){
+    return `${this.nom} est un ${this.race} mesurant ${this.taille} cm`
+  }
+  aboyer(){
+    let result = "Whouaf Whouaf !";
+    if (Number(this.taille)>50){
+      result = "Grrr ! Grrr !";
+    }
+    return result;
+  }
+}
+
+const crockdur = new Chien("Crockdur", "mâtin de Naples", 75);
+// "Crockdur est un mâtin de Naples mesurant 75 cm"
+console.log(crockdur.decrire());
+// "Tiens, un chat ! Crockdur aboie : Grrr ! Grrr !"
+console.log(`Tiens, un chat ! ${crockdur.nom} aboie : ${crockdur.aboyer()}`);
+
+const milou = new Chien("Milou", "fox-terrier", 26);
+// "Milou est un fox-terrier mesurant 26 cm"
+console.log(milou.decrire());
+// "Tiens, un chat ! Milou aboie : Wouaf ! Wouaf !"
+console.log(`Tiens, un chat ! ${milou.nom} aboie : ${milou.aboyer()}`);
+
