d81730dd8b1c50c9cd40da6d65cd2e0e972b3573
B4PT <30923509+B4PT@users.noreply.github.com>
2020-01-17 20:01:37 +0100

    Update save.js

--- save.js
+++ save.js
@@ -14,4 +14,12 @@ console.log(compterVoyelles("RadAr")); // 2
 console.log(compterVoyelles("Tic et Tac")); // 3
 console.log(compterVoyelles("Oasis Oasis Oh")); // 7
 
+// Affectation d'une fonction anonyme à la variable maVariable
+const maVariable = (param1, param2, ...) => {
+  // Instructions pouvant utiliser param1, param2, ...
+}
+
+// Appel de la fonction anonyme
+// param1 reçoit la valeur de arg1, param2 la valeur de arg2, ...
+maVariable(arg1, arg2, ...);
 
